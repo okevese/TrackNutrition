64cf9daf991bc1b6efbcc2b8f23fe2754f0bcd35
oke vese <okevese@gmail.com>
2019-05-08 11:50:55 +0100

    add and update migration

--- TrackNutrition/Data/ApplicationDbContext.cs
+++ TrackNutrition/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrackNutrition.Models;
 
 namespace TrackNutrition.Data
 {
@@ -11,6 +12,13 @@ namespace TrackNutrition.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<DailyNutrientItem> Nutrients { get; set; }
+        // Called when your model is first created to build the model and it's mappings in memory    
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ TrackNutrition/Data/Migrations/20190508092159_AddDailyNutrients.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrackNutrition.Data.Migrations
+{
+    public partial class AddDailyNutrients : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nutrients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Proteins = table.Column<string>(nullable: true),
+                    ProteinQuantity = table.Column<int>(nullable: false),
+                    Fruits = table.Column<string>(nullable: true),
+                    FruitQuantity = table.Column<int>(nullable: false),
+                    Oils = table.Column<string>(nullable: true),
+                    OilQuantity = table.Column<int>(nullable: false),
+                    Dairy = table.Column<string>(nullable: true),
+                    DairyQuantity = table.Column<int>(nullable: false),
+                    Grains = table.Column<string>(nullable: true),
+                    GrainQuantity = table.Column<int>(nullable: false),
+                    Vegetables = table.Column<string>(nullable: true),
+                    VegetableQuantity = table.Column<int>(nullable: false),
+                    Calories = table.Column<int>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutrients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nutrients");
+        }
+    }
+}

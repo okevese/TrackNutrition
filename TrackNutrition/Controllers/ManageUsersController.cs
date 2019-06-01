d04356c43bd9d5ae0d3a1225a982084aa1f8ddaf
oke vese <okevese@gmail.com>
2019-06-01 23:40:30 +0100

    add manage users controller

---
+++ TrackNutrition/Controllers/ManageUsersController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using TrackNutrition.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrackNutrition.Controllers 
+{
+    
+}
